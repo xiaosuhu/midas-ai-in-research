b86cf739fc45751d3f2e12731a316a55d4a64d1d
Frank <xiaosuhu@outlook.com>
2025-11-04 11:47:34 -0500

    first build

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,8 @@
 # -- Project info -----------------------------------------------------
-project = "MIDAS AI in Research"
-author = "Frank Hu @ MIDAS, University of Michigan"
-html_title = "MIDAS AI in Research"
-release = "0.1"
+project = 'MIDAS AI in Research'
+copyright = '2025, Frank Hu'
+author = 'Frank Hu'
+release = '0.1'
 
 # -- General config ---------------------------------------------------
 extensions = [
